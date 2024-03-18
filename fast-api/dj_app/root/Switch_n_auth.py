a564e1381755dc26bb6f99b7d76fd353e4f6c63d
ujazishere <ujasvaghani@gmail.com>
2024-03-17 23:24:21 -0400

    initial

---
+++ fast-api/dj_app/root/Switch_n_auth.py
@@ -0,0 +1,12 @@
+
+
+EC2_location = "Local Server"
+
+run_lengthy_web_scrape = False
+
+
+public_email = 'publicuj@gmail.com'
+smtp_password = "dsxi rywz jmxn qwiz"
+
+personal_email = 'ujasvaghani@gmail.com'
+ismail_email = 'ismailsakhani879@gmail.comm'
